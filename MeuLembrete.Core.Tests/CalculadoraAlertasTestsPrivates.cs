f62b0d6ed14878f989a1ea6687d4da22972cfd3f
Carlos André <carlos_andre.1@hotmail.com>
2023-09-26 11:19:48 -0300

    tests: melhoria do ambiente de testes, isolamento de algumas classes.

--- MeuLembrete.Core.Tests/CalculadoraAlertasTestsPrivates.cs
+++ MeuLembrete.Core.Tests/CalculadoraAlertasTestsPrivates.cs
@@ -1,4 +1,6 @@
-﻿namespace MeuLembreteCore.Tests
+﻿using MeuLembrete.Core.Services.Handlers;
+
+namespace MeuLembreteCore.Tests
 {
     public class CalculadoraAlertasTestsPrivates : CalculadoraAlertas
     {
@@ -11,6 +13,23 @@
 
             Assert.StrictEqual(avaliador2, avaliador1);
 
+        }
+    }
+
+    public class CalculadoraAlertasTests
+    {
+        [Fact]
+        public void CalculadoraAlertas_DeveRetornarLembretesDoDia()
+        {
+            ILembreteService lembreteService = new LembreteServiceMock();
+            LembreteCachedRepository lembreteCachedRepository = new LembreteCachedRepository(lembreteService);
+
+            lembreteCachedRepository.UpdateCache();
+            CalculadoraAlertas calculadoraAlertas = new CalculadoraAlertas();
+            IEnumerable<Lembrete> lembretes = calculadoraAlertas.RetornarLembretesNoHorario(lembreteCachedRepository.LembretesCache, DateTime.Now, false);
+
+            
+            //Assert.True(lembretes.All(l => l.Alertas.Any(a => a.)));
         }
     }
 
